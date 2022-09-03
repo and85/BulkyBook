d47007ad21059e72b8dac5064811e0dfdf64d015
and85 <andreytkach85@gmail.com>
2022-09-03 13:51:18 +0200

    added unit of work and CoverType

--- BulkyBook.DataAccess/Repository/CategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -1,11 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BulkyBook.DataAccess.Repository
 {
     public class CategoryRepository : Repository<Category>, ICategoryRepository
@@ -15,11 +10,6 @@ namespace BulkyBook.DataAccess.Repository
         public CategoryRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
-        }
-
-        public void Save()
-        {
-            _db.SaveChanges();
         }
 
         public void Update(Category obj)
---
+++ BulkyBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,127 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CoverType obj)
+        {
+            ValidateModel(obj);
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Covert Type created successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var covertTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (covertTypeFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(covertTypeFromDb);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CoverType obj)
+        {
+            ValidateModel(obj);
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var covertTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (covertTypeFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(covertTypeFromDb);
+        }
+
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+
+            return RedirectToAction("Index");
+        }
+
+        private void ValidateModel(CoverType obj)
+        {
+            if (obj.Name == "CoverType" || obj.Name == "Name")
+            {
+                ModelState.AddModelError("name", "The CoverType shoud have a meaningful name.");
+            }
+        }
+    }
+}
